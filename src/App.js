89c27b0e25af59d0965a510891e3db99798d63d3
Anthourn <80663362+Anthourn@users.noreply.github.com>
2021-06-06 12:34:26 -0400

    created css cards, mobile friendly with acccessibilty

--- src/App.js
+++ src/App.js
@@ -5,6 +5,13 @@ function App() {
   return (
     <div className="App">
       <h1> Avengers initiative </h1>
+      <div class = 'card'>
+      <div class = 'card-content'>
+      <h2 class = 'card-title'>Avenger #</h2>
+      <p class = 'card-body'> "There was an idea, Stark knows this, called the Avengers Initiative. The idea was to bring together a group of remarkable people, to see if they could become something more. To see if they could work together when we needed them to. To fight the battles that we never could. </p>
+      <a href = '#' class = 'button'>learn more </a>
+      </div>
+      </div>
     </div>
   );
 }
