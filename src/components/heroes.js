3e360518862d2e7fe0d4dffedd13bb61d635b33f
Anthourn <80663362+Anthourn@users.noreply.github.com>
2021-06-06 22:55:07 -0400

    api data displaying on page

--- src/components/heroes.js
+++ src/components/heroes.js
@@ -3,9 +3,12 @@ import axios from 'axios'
 import React, {useState} from 'react'
 // let heroList = ''
 export const Heroes = () => {
+    const [heroList, setHero] = useState(0)
     axios.get('https://gateway.marvel.com:443/v1/public/characters?apikey=a723f1e04ab084a12d0437077b3e90cd').then((response) =>{
-        console.log(response.data)
-     // const [heroList, setHero] = useState(0)
+        // console.log(response.data)
+        setHero(response.data)
+        console.log(heroList)
+
     }
 
         )
@@ -13,7 +16,7 @@ export const Heroes = () => {
         console.log('test')
         return(
             <div>
-            <h1>test</h1>
+            <h1>{heroList.data.results[0].name}</h1>
             <p> </p>
             </div>
         )
