722db80141091f70f045c03dfe70fa6f7053c0ca
Anthourn <80663362+Anthourn@users.noreply.github.com>
2021-06-08 15:40:15 -0400

    passing hooks to sibling components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
-import {Heroes} from './components/heroes'
+import axios from 'axios'
+import React, {useState, useEffect} from 'react'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import {Display} from './components/display'
+import Homearray from './components/array.js'
 function App() {
+
+    const [hero, setHero] = useState('Nick Fury')
+    const getHeroes = (name) => {
+        console.log('test')
+        axios.get(`https://gateway.marvel.com:443/v1/public/characters?name=${name}&apikey=a723f1e04ab084a12d0437077b3e90cd
+`).then((response) =>{
+            console.log(response.data)
+
+
+        })}
+    useEffect(() => {
+        // getHeroes(hero)
+        console.log(hero)
+    }, [] )
   return (
+      <Router>
     <div className="App">
       <h1> Avengers initiative </h1>
-      <div className = 'card'>
-      <div className = 'card-content'>
-      <h2 className = 'card-title'>Avenger #</h2>
-      <p className = 'card-body'> "There was an idea, Stark knows this, called the Avengers Initiative. The idea was to bring together a group of remarkable people, to see if they could become something more. To see if they could work together when we needed them to. To fight the battles that we never could. </p>
-      <a href = '#' className = 'button'>learn more </a>
-      </div>
-      </div>
-      <Heroes/>
+
+      <Switch>
+
+
+
+        <Route path = '/display'>
+        <Display getHeroes={getHeroes} hero={hero}/>
+        </Route>
+
+
+        <Route exact path = '/'>
+        <Homearray setHero={setHero} hero={hero}/>
+        </Route>
+
+       </Switch>
+
+
     </div>
+    </Router>
   );
 }
 
--- src/components/heroes.js
+++ src/components/heroes.js
@@ -1,23 +1,30 @@
-import react from 'react'
+// import react from 'react'
 import axios from 'axios'
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 // let heroList = ''
 export const Heroes = () => {
-    const [heroList, setHero] = useState(0)
-    axios.get('https://gateway.marvel.com:443/v1/public/characters?apikey=a723f1e04ab084a12d0437077b3e90cd').then((response) =>{
-        // console.log(response.data)
-        setHero(response.data)
-        console.log(heroList)
 
-    }
-
-        )
-
+    const getHeroes = () => {
         console.log('test')
+        axios.get('https://gateway.marvel.com:443/v1/public/characters?apikey=a723f1e04ab084a12d0437077b3e90cd').then((response) =>{
+            // console.log(response.data)
+            setHero(response.data)
+            console.log(heroList)
+
+        })}
+
+    // useEffect(() => {
+    //
+    // }, [] )
+
+
         return(
             <div>
-            <h1>{heroList.data.results[0].name}</h1>
-            <p> </p>
+
+            <p>
+
+
+             </p>
             </div>
         )
 
